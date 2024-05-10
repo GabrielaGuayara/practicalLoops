32123794668e92c7e2feefa2494c912e51e6ac37
Gabriela Guayara <gabriela.guayara@gmail.com>
2024-05-10 16:16:04 -0400

    Utilized For Loop for second exercise

--- app.js
+++ app.js
@@ -1,15 +1,44 @@
 //PART ONE: FIZZ BUZZ
-let n = 100;
-for(let i=1; i<= n; i++){
-    if((i%3)===0){
-        console.log("Fizz")
+// let num = 100;
+// for(let i=1; i<= num; i++){
+//     if((i%3)===0){
+//         console.log("Fizz")
+//     }
+//     else if((i%5)===0){
+//         console.log("Buzz")
+//     }
+//     else if((i%3)&&(i%5)===0){
+//         console.log("Fizz Buzz")
+//     }
+//     else{
+//         console.log(i)}
+// }
+
+
+//PART TWO: PRIME TIME
+let n=5
+let counter = 10;
+if(n>1){
+    for(let i=n; i<= counter; i++){
+        n++;
+        if(!(n%i===0)){
+            console.log(i)
+            break;
+        }
     }
-    else if((i%5)===0){
-        console.log("Buzz")
-    }
-    else if((i%3)&&(i%5)===0){
-        console.log("Fizz Buzz")
-    }
-    else{
-        console.log(i)}
 }
+
+//PART THREE: FEELING LOOPY
+const data = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26"
+
+for(j=0; j<= data.length; j++){
+    let newChar = data[j]
+
+    if(newChar == ","){
+        console.log(newChar)
+    }
+}
+
+
+
+
